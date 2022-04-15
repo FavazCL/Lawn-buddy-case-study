774559bdde31f8521c91e4415216906b9fff7ebe
Felipe Vergara <favazcl@gmail.com>
2022-04-15 19:29:02 -0400

    clean files

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,14 +1,6 @@
-// Copyright (c) 2022, Very Good Ventures
-// https://verygood.ventures
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:lawn_budy_case_study/counter/counter.dart';
-import 'package:lawn_budy_case_study/l10n/l10n.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
@@ -23,10 +15,8 @@ class App extends StatelessWidget {
         ),
       ),
       localizationsDelegates: const [
-        AppLocalizations.delegate,
         GlobalMaterialLocalizations.delegate,
       ],
-      supportedLocales: AppLocalizations.supportedLocales,
       home: const CounterPage(),
     );
   }
--- lib/counter/view/counter_page.dart
+++ lib/counter/view/counter_page.dart
@@ -8,7 +8,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:lawn_budy_case_study/counter/counter.dart';
-import 'package:lawn_budy_case_study/l10n/l10n.dart';
 
 class CounterPage extends StatelessWidget {
   const CounterPage({Key? key}) : super(key: key);
@@ -27,9 +26,8 @@ class CounterView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final l10n = context.l10n;
     return Scaffold(
-      appBar: AppBar(title: Text(l10n.counterAppBarTitle)),
+      appBar: AppBar(title: Text('')),
       body: const Center(child: CounterText()),
       floatingActionButton: Column(
         mainAxisAlignment: MainAxisAlignment.end,

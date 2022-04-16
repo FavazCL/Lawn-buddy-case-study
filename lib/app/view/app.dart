a95531a0d91efd4d96d3db5be7283aea4d43fb9d
Felipe Vergara <favazcl@gmail.com>
2022-04-15 20:29:38 -0400

    add in feature layer the repositories

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,9 +1,29 @@
+import 'package:domain/domain.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:lawn_budy_case_study/counter/counter.dart';
 
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider<EmployeeRepository>.value(
+          value: EmployeeRepository(),
+        ),
+        RepositoryProvider<CompanyRepository>.value(
+          value: CompanyRepository(),
+        ),
+      ],
+      child: const AppView(),
+    );
+  }
+}
+
+class AppView extends StatelessWidget {
+  const AppView({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
--- packages/domain/lib/domain.dart
+++ packages/domain/lib/domain.dart
@@ -1,1 +1,4 @@
 library domain;
+
+export 'src/company_repository/company_repository.dart';
+export 'src/employee_repository/employee_repository.dart';
